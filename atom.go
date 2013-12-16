d190578d48f71a3f4cac8cc211d501d1006b32ca
Tony Fabeen <tony.fabeen@gmail.com>
2013-12-16 18:45:23 -0200

    Added .vagrant to .gitignore. Light refactoring on atom.go

--- atom.go
+++ atom.go
@@ -3,24 +3,46 @@ package main
 import (
   linuxproc "github.com/c9s/goprocinfo/linux"
   "os/exec"
+  "strconv"
   "log"
 )
 
-type MachineInfo struct{
-  cpus int
-  memory int
+type Machine struct{
+  cpus string
+  memory string
 }
+
+func (machine *Machine) AvailableCpus() string {
+  nproc, _ := exec.Command("nproc").Output()
+  cpus := string(nproc)
+  machine.cpus = cpus
+  return machine.cpus
+}
+
+func (machine *Machine) AvailableMemory() string {
+  memInfo, err :=  linuxproc.ReadMemInfo("/proc/meminfo")
+  if err != nil {
+    log.Fatal("Error on Read MemInfo")
+  }
+  machine.memory = strconv.FormatUint(memInfo["MemFree"], 10)
+  return machine.memory
+}
+
+func MachineInfo() *Machine {
+  machine := new(Machine)
+  machine.AvailableCpus()
+  machine.AvailableMemory()
+  return machine
+}
+
+
 
 func main(){
 
-  cpus, _ := exec.Command("nproc").Output()
-  memInfo, err :=  linuxproc.ReadMemInfo("/proc/meminfo")
-  if err != nil {
-    log.Println("Error on Read MemInfo")
-  }
+  machine := MachineInfo()
 
-  log.Printf("CPU Core: %s", cpus)
-  log.Println("Meminfo: ", memInfo["MemFree"])//.Free)
+  log.Println("Machine Info> ", machine)
 
-
+  log.Printf("CPU Core: %s", machine.cpus)
+  log.Println("Meminfo: ", machine.memory)
 }
